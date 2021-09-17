8e58ec7888e595ff211a0af07a36518571c27b1a
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-17 02:53:15 -0500

    create  migration category

--- database/migrations/2021_09_17_074654_create_categories_table.php
+++ database/migrations/2021_09_17_074654_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('cat_name');
+            $table->string('cat_observations');
             $table->timestamps();
         });
     }
