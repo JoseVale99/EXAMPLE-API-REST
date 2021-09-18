9b2db38f56102711a8ad32572fef8fd83dfe7a3a
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-17 20:01:09 -0500

    API show for id

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/category',[CategoryController::class, 'index']);
+Route::get('/category/{id}',[CategoryController::class, 'show']);
