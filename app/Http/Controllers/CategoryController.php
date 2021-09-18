6287ab4bf2e2fd55771267fee738f2c900658b93
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-17 20:01:00 -0500

    routes for show ID

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,4 +9,21 @@ class CategoryController extends Controller
     public function index(){
         return response()->json(Category::all(),200);
     }
+
+    public function show($id){
+        $category = Category::find($id);
+        
+        if (!$category){
+            return response()->
+            json(
+                ['error'=>'404 Not Found'],404);
+        }
+            return response()->json(
+        [$category,'message' => 'data show success!']
+        );
+    }
+    
+
 }
+
+
