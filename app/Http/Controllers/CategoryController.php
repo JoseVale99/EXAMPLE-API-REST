e47c0da67e69f9f1a14fcf09bbb188025ed69797
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-17 19:27:20 -0500

    quit import

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,10 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
 use App\Models\Category;
 class CategoryController extends Controller
 {
