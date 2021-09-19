8fb9ae6408dfe26aacb3b6b402806f139995c29c
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-18 20:11:28 -0500

    API complet!

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -8,50 +9,56 @@ class CategoryController extends Controller
 {
     //
 
-    public function index(){
-        return response()->json(Category::all(),200);
+    public function index()
+    {
+        return response()->json(Category::all(), 200);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $category = Category::find($id);
-        
-        if (!$category){
-            return response()->
-            json(
-                ['error'=>'404 Not Found'],404);
-        }
+
+        if (!$category) {
             return response()->json(
-        [$category,'message' => 'data show success!']
+                    ['error' => '404 Not Found'],
+                    404
+                );
+        }
+        return response()->json(
+            [$category, 'message' => 'data show success!']
         );
     }
-    public function create(Request $request){
+    public function create(Request $request)
+    {
 
         $category = Category::create($request->all());
 
         return response()->json(
-            [$category,'message' => 'data insert success!']
+            [$category, 'message' => 'data insert success!']
         );
-    } 
-
-    public function update(Request $request,$id){
-
-        $category = Category::find($id);
-       if (!$category){
-        return response()->json(['error'=>'404 Not Found'],404);
-       }
-      $category->update($request->all());
-      return response()->json(
-          [$category,'message' => 'data update success!']); 
     }
-    public function delete($id){
+
+    public function update(Request $request, $id)
+    {
+
         $category = Category::find($id);
-        if (!$category){
-            return response()->json(['error'=>'404 Not Found'],404);
-           }
-           $category->delete();
-           return response()->json(
-            [$category,'message' => 'data delete success!']);  
+        if (!$category) {
+            return response()->json(['error' => '404 Not Found'], 404);
+        }
+        $category->update($request->all());
+        return response()->json(
+            [$category, 'message' => 'data update success!']
+        );
+    }
+    public function delete($id)
+    {
+        $category = Category::find($id);
+        if (!$category) {
+            return response()->json(['error' => '404 Not Found'], 404);
+        }
+        $category->delete();
+        return response()->json(
+            [$category, 'message' => 'data delete success!']
+        );
     }
 }
-
-
